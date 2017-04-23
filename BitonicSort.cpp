b0d91ca04afcb9a952aa6534fb75cf377be5f413
Aryan Gupta <kuantum.freak@gmail.com>
2017-04-23 18:46:30 -0400

    Code BitonicSort

--- BitonicSort.cpp
+++ BitonicSort.cpp
@@ -17,3 +17,43 @@
 #include "info.h"
 
 #include ".\inc\main.h"
+
+namespace SortAlg {
+	template <typename ITER>
+	void BitonicSort(ITER start, ITER end) {
+		BitonicSortHelper(true, start, end);
+	}
+	
+	template <typename ITER>
+	void BitonicSortHelper(bool up, ITER start, ITER end) {
+		if(end - start <= 1)
+			return;
+		
+		int half = (end - start) / 2
+		BitonicSortHelper(true, start, end - half);
+		BitonicSortHelper(false, start + half, end);
+		BitonicSortMerge(up, start, end);
+	}
+	
+	template <typename ITER>
+	void BitonicSortMerge(bool up, ITER start, ITER end) {
+		if(end - start <= 1)
+			return;
+		
+		BitonicSortCompare(up, start, end);
+		
+		int half = (end - start) / 2
+		BitonicSortMerge(up, start, end - half);
+		BitonicSortMerge(up, start + half, end);
+	}
+	
+	template <typename ITER>
+	void BitonicSortCompare(bool up, ITER start, ITER end) {
+		int half = (end - start) / 2
+		
+		for(; start < end - half; start++) {
+			if((*start > *(start + half)) == up)
+				std::iter_swap(start, start + half);
+		}
+	}
+}
