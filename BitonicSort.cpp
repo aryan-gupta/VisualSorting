8aa277632416b4befadc59c3445a48228655ef22
Aryan Gupta <kuantum.freak@gmail.com>
2017-04-23 19:10:17 -0400

    BitonicSort Visual Version

--- BitonicSort.cpp
+++ BitonicSort.cpp
@@ -58,3 +58,47 @@ namespace SortAlg {
 		BitonicSortHelper(true, start, end);
 	}
 }
+
+namespace SortAlgVis {	
+	template <typename ITER>
+	void BitonicSortCompare(bool up, ITER start, ITER end) {
+		int half = (end - start) / 2;
+		
+		for(; start < end - half; start++) {
+			::gWindow->render({start});
+			if((*start > *(start + half)) == up) {
+				std::iter_swap(start, start + half);
+				::gWindow->render({start});
+			}
+		}
+	}
+	
+	template <typename ITER>
+	void BitonicSortMerge(bool up, ITER start, ITER end) {
+		if(end - start <= 1)
+			return;
+		
+		BitonicSortCompare(up, start, end);
+		
+		int half = (end - start) / 2;
+		BitonicSortMerge(up, start, end - half);
+		BitonicSortMerge(up, start + half, end);
+	}
+	
+	
+	template <typename ITER>
+	void BitonicSortHelper(bool up, ITER start, ITER end) {
+		if(end - start <= 1)
+			return;
+		
+		int half = (end - start) / 2;
+		BitonicSortHelper(true, start, end - half);
+		BitonicSortHelper(false, start + half, end);
+		BitonicSortMerge(up, start, end);
+	}
+	
+	template <typename ITER>
+	void BitonicSort(ITER start, ITER end) {
+		BitonicSortHelper(true, start, end);
+	}
+}
