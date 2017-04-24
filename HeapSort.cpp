3ee3395c94cc024c3a8a5306e7bef8dc2dcd39e6
Aryan Gupta <kuantum.freak@gmail.com>
2017-04-24 09:36:05 -0400

    Not Working HeapSort

--- HeapSort.cpp
+++ HeapSort.cpp
@@ -17,8 +17,48 @@
 #include "info.h"
 
 namespace SortAlg {
+		template <typename ITER>
+	void Heapify(ITER start, ITER end) {
+	
+	}
+	
 	template <typename ITER, typename FUNC>
-	void HeapSort(ITER start, ITER end, FUNC cmp) {
+	void Heapify(ITER first, ITER start, ITER end) {
+		ITER j = (start - first) * 2;
+		typename ITER::value_type tmp = *start;
 		
+		while(j < end) {
+			if(j < end && *(j + 1) > *(j)) {
+				j++;
+			}
+			
+			if(tmp > *(j)) {
+				break;
+			} //else
+			
+			if(tmp <= *(j)) {
+				*(j / 2) = *(j);
+				j = 2 * j;
+			}
+		}
+		*(j / 2) = tmp;
+	}
+	
+	template <typename ITERC>
+	void HeapBuild(ITER start, ITER end) {
+		int mid = (end - start)/2;
+		for(; mid >= 1; --mid) {
+			Heapify(start, start + mid, end);
+		}
+	}
+	
+	template <typename ITER>
+	void HeapSort(ITER start, ITER end) {
+		HeapBuild(start, end);
+		Heapify();
+		
+		end--;
+		
+		while(end > start)
 	}
 }
