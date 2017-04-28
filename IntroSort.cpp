8ee31136545acfa504d77f05c5b525558aead970
Aryan Gupta <kuantum.freak@gmail.com>
2017-04-28 10:54:04 -0400

    Code IntroSort

--- IntroSort.cpp
+++ IntroSort.cpp
@@ -17,3 +17,27 @@
 #include "info.h"
 
 #include <cmath>
+
+namespace SortAlg {
+	template <typename ITER>
+	void IntroSortHelper(ITER start, ITER end, unsigned max) {
+		std::size_t size = std::distance(start, end);
+		
+		if(size <= 1)
+			return;
+		
+		if(max == 0) {
+			SortAlg::HeapSort(start, end);
+		} else {
+			std::size_t half = size / 2;
+			IntroSortHelper(start, end - half);
+			IntroSortHelper(start + half, end);
+		}
+	}
+	
+	template <typename ITERC>
+	void IntroSort(ITER start, ITER end) {
+		unsigned max = log(std::distance(start, end)) * 2;
+		IntroSortHelper(start, end, max);
+	}
+}
