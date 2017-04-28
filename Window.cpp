a83310c81d5d4cade5b2a44c49b7e19141370cbc
Aryan Gupta <kuantum.freak@gmail.com>
2017-04-27 21:54:43 -0400

    Update Menu

--- Window.cpp
+++ Window.cpp
@@ -118,10 +118,11 @@ int Window::askSort() {
 		 << "4.  Gnome Sort" << endl
 		 << "5.  Stoogle Sort" << endl
 		 << "6.  Radix LSD Sort" << endl
-		 << "7.  Radix MSD Sort" << endl
+		 << "7.  Radix MSD Sort - Not Working" << endl
 		 << "8.  Bitonic Sort" << endl
 		 << "9.  CockTail Sort" << endl
 		 << "10. Shell Sort" << endl
+		 << "11. Merge Sort" << endl
 		 << endl;
 		 
 	int c;
