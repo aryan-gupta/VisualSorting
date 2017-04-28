06a9f2edc88de05694dcf3ab9dec866f6989551c
Aryan Gupta <kuantum.freak@gmail.com>
2017-04-27 21:22:56 -0400

    Add Comments to StoogleSort

--- StoogleSort.cpp
+++ StoogleSort.cpp
@@ -19,18 +19,18 @@
 namespace SortAlg {
 	template <typename ITER, typename FUNC>
 	void StoogleSort(ITER start, ITER end, FUNC cmp) {
-		end--;
+		end--; // decrement end because ALG sorts [start, end]
 		
-		if(!cmp(*start, *end)) {
-			std::iter_swap(start, end);
-		}
+		if(!cmp(*start, *end)) 
+			std::iter_swap(start, end); // swap if they are our of order
 		
-		if(end - start + 1 > 2) {
-			std::size_t prt = (end - start + 1)/3;
+		if(end - start + 1 > 2) { // make sure there is somthing to sort
+			std::size_t prt = (end - start + 1)/3; // get 2/3 partition
 			
-			StoogleSort(start, end - prt + 1, cmp);
-			StoogleSort(start + prt, end + 1, cmp);
-			StoogleSort(start, end - prt + 1, cmp);
+			// we are adding 1 to the end because ALG sorts [start, end]
+			StoogleSort(start, end - prt + 1, cmp); // sort first 2/3
+			StoogleSort(start + prt, end + 1, cmp); // sort second 2/3
+			StoogleSort(start, end - prt + 1, cmp); // resort first 2/3
 		}
 	}
 }
