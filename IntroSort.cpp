594ec0e7b017576068d1d13d16f3bf3c8bd06acc
Aryan Gupta <kuantum.freak@gmail.com>
2017-04-29 22:22:08 -0400

    Modify to Align with Original ALG

--- IntroSort.cpp
+++ IntroSort.cpp
@@ -61,19 +61,18 @@ namespace SortAlgVis {
 	
 	template <typename ITER, typename FUNC>
 	void IntroSortHelper(ITER start, ITER end, unsigned max, FUNC cmp) { 		
-		if(std::distance(start, end) < 16)
-			return;
-		
-		if(max == 0) {
-			SortAlgVis::HeapSort(start, end, cmp);
+		while(std::distance(start, end) > 10) {
+			if(max == 0) {
+				SortAlgVis::HeapSort(start, end, cmp);
+				::gWindow->render();
+				return;
+			}
+			--max;
+			auto prt = PartMedOf3(start, end, cmp);
+			IntroSortHelper(prt, end, max, cmp);
+			end = prt;
 			::gWindow->render();
-			return;
 		}
-		
-		auto prt = PartMedOf3(start, end, cmp);
-		IntroSortHelper(start, prt, max - 1, cmp);
-		IntroSortHelper(prt, end, max - 1, cmp);
-		::gWindow->render();
 	}
 	
 	template <typename ITER, typename FUNC>
