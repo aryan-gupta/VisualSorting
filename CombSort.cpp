cca18739de07e46220494d800543d73b8704a691
Aryan Gupta <kuantum.freak@gmail.com>
2017-04-30 19:59:07 -0400

    Code CombSort

--- CombSort.cpp
+++ CombSort.cpp
@@ -16,3 +16,31 @@
  */
 #include "info.h"
 
+namespace SortAlg {
+	template <typename ITER, typename FUNC>
+	void SelectionSort(ITER start, ITER end, FUNC cmp) {
+		std::size_t gap = std::distance(end, start);
+		shrink = 1.3;
+		bool sorted = false;
+		
+		while(!sorted) {
+			gap = gap / shrink;
+			
+			if(gap > 1) {
+				sorted = false;
+			} else {
+				gap = 1;
+				sorted = true;
+			}
+			
+			std::size_t i = 0;
+			while(i + gap < std::distance(end, start)) {
+				if(*(start + i) > *(start + i + gap)) {
+					std::iter_swap(start + i, start + i + gap);
+					sorted = false;
+				}
+				++i;
+			}
+		}
+	}
+}
