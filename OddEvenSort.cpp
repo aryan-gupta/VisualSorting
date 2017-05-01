ca0dfca9d993f278a6d9f3461733e243124bb363
Aryan Gupta <kuantum.freak@gmail.com>
2017-04-30 21:31:06 -0400

    Code Visual Version

--- OddEvenSort.cpp
+++ OddEvenSort.cpp
@@ -39,3 +39,33 @@ namespace SortAlg {
 		}
 	}
 }
+
+namespace SortAlgVis {
+	template <typename ITER>
+	void OddEvenSort(ITER start, ITER end) {
+		bool sorted = false;
+		while(!sorted) {
+			sorted = true;
+			
+			::gWindow->render({start});
+			for(std::size_t i = 0; i < std::distance(start, end) - 1; i += 2) {
+				::gWindow->render({start + i, start + i + 1});
+				if(*(start + i) > *(start + i + 1)) {
+					std::iter_swap(start + i, start + i + 1);
+					::gWindow->render({start + i, start + i + 1});
+					sorted = false;
+				}
+			}
+			
+			::gWindow->render({start});
+			for(std::size_t i = 1; i < std::distance(start, end) - 1; i += 2) {
+				::gWindow->render({start + i, start + i + 1});
+				if(*(start + i) > *(start + i + 1)) {
+					std::iter_swap(start + i, start + i + 1);
+					::gWindow->render({start + i, start + i + 1});
+					sorted = false;
+				}
+			}
+		}
+	}
+}
