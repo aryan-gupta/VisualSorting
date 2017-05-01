2f9fe6049f83e22a3f332da5b5447ab79c6fb85c
Aryan Gupta <kuantum.freak@gmail.com>
2017-04-30 21:25:17 -0400

    Code OdEvenSort

--- OddEvenSort.cpp
+++ OddEvenSort.cpp
@@ -16,3 +16,24 @@
  */
 #include "info.h"
 
+namespace SortAlg {
+	template <typename ITER>
+	void SelectionSort(ITER start, ITER end) {
+		bool sorted = false;
+		while(!sorted) {
+			for(std::size_t i = 0; i < std::distance(start, end) - 1; i += 2) {
+				if(*(start + i) > *(start + i + 1)) {
+					std::iter_swap(start + i, start + i + 1);
+					sorted = false;
+				}
+			}
+			
+			for(std::size_t i = 1; i < std::distance(start, end) - 1; i += 2) {
+				if(*(start + i) > *(start + i + 1)) {
+					std::iter_swap(start + i, start + i + 1);
+					sorted = false;
+				}
+			}
+		}
+	}
+}
