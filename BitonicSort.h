0dbb2b8ef7df179aee897e86aee768801baa4944
Aryan Gupta <kuantum.freak@gmail.com>
2017-05-11 17:58:41 -0400

    BitonicSort NONCMP

--- BitonicSort.h
+++ BitonicSort.h
@@ -39,7 +39,6 @@ namespace SortAlg {
 		BitonicSortMerge(up, start + half, end, cmp);
 	}
 	
-	
 	template <typename ITER, typename FUNC>
 	void BitonicSortHelper(bool up, ITER start, ITER end, FUNC cmp) {
 		if(end - start <= 1)
@@ -54,6 +53,47 @@ namespace SortAlg {
 	template <typename ITER, typename FUNC>
 	void BitonicSort(ITER start, ITER end, FUNC cmp) {
 		BitonicSortHelper(true, start, end, cmp);
+	}
+	
+	// ==================== NO COMPARE FUNC ====================================
+	
+		template <typename ITER>
+	void BitonicSortCompare(bool up, ITER start, ITER end) {
+		int half = (end - start) / 2;
+		
+		for(; start < end - half; start++) {
+			if(*start > *(start + half) == up)
+				std::iter_swap(start, start + half);
+		}
+	}
+	
+	template <typename ITER>
+	void BitonicSortMerge(bool up, ITER start, ITER end) {
+		if(end - start <= 1)
+			return;
+		
+		BitonicSortCompare(up, start, end);
+		
+		int half = (end - start) / 2;
+		BitonicSortMerge(up, start, end - half);
+		BitonicSortMerge(up, start + half, end);
+	}
+	
+	
+	template <typename ITER>
+	void BitonicSortHelper(bool up, ITER start, ITER end) {
+		if(end - start <= 1)
+			return;
+		
+		int half = (end - start) / 2;
+		BitonicSortHelper(true, start, end - half);
+		BitonicSortHelper(false, start + half, end);
+		BitonicSortMerge(up, start, end);
+	}
+	
+	template <typename ITER>
+	void BitonicSort(ITER start, ITER end, bool up) {
+		BitonicSortHelper(up, start, end);
 	}
 }
 
