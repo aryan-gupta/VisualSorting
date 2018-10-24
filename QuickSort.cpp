d014c84de69df2acbe2a3a24e3d1fb18d91a7218
Aryan Gupta <kuantum.Freak@gmail.com>
2018-10-24 07:00:21 -0400

    Rename part pointer
    The old naming scheme called the partition pointer ptr and the for loop pointer pntr and was confusing. Renamed ptr to part and loop pointer to pntr

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -19,17 +19,17 @@
 namespace SortAlg {
 	template <typename ITER, typename FUNC>
 	ITER part(ITER start, ITER end, FUNC cmp) {
-		ITER prt = start; // our parted pointer so [start, prt) is less than the pivot
+		ITER part = start; // our parted pointer so [start, part) is less than the pivot
 		
-		for(ITER pntr = start; pntr < end; ++pntr) { // go through the array except the pivot point
-			if(cmp(*pntr, *end)) {         // if our pointer is less than our pivot
-				std::iter_swap(prt, pntr); // swap so its its before our partition
-				prt++;                     // increment partition
+		for(ITER ptr = start; ptr < end; ++ptr) { // go through the array except the pivot point
+			if(cmp(*ptr, *end)) {         // if our pointer is less than our pivot
+				std::iter_swap(part, ptr); // swap so its its before our partition
+				part++;                     // increment partition
 			}
 		}
 		
-		std::iter_swap(prt, end); // swap our pivot with the first val more than pivot
-		return prt; // return out pivot
+		std::iter_swap(part, end); // swap our pivot with the first val more than pivot
+		return part; // return out pivot
 	}
 	
 	template <typename ITER, typename FUNC>
